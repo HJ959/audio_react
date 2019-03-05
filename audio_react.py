00b6f34b9d777105b3c6251afea7a2b226f2fd2d
HJ959 <henryjaames@gmail.com>
2019-03-05 11:01:29 +0000

    Updated with cleaner functions and webcam function

--- audio_react.py
+++ audio_react.py
@@ -10,8 +10,11 @@ import audioop
 import time
 import random
 #import python3 third party
+import numpy as np
 import pyaudio
-import pygame
+import pygame as pyg
+import cv2
+
 # -----------------------------------------------------------------------------
 def rmsFunction():
     # Creates a generator that can iterate rms values
@@ -20,74 +23,135 @@ def rmsFunction():
     CHANNELS = 2
     RATE = 44100
     colourMax = 255
+
     p = pyaudio.PyAudio()
 
-    stream = p.open(format=p.get_format_from_width(WIDTH),
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    output=True,
-                    frames_per_buffer=CHUNK)
-    time.sleep(1)
-    while True:
-        data = stream.read(CHUNK, exception_on_overflow = False)
-        #stream.write(data, CHUNK)
-        rms = audioop.rms(data, 2)
-        # Scale the rms value to be within 0-255
-        rmsS = rms * 0.014
-        if rmsS < colourMax:
-            yield rmsS
+    try:
+        stream = p.open(format=p.get_format_from_width(WIDTH),
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        output=True,
+                        frames_per_buffer=CHUNK)
 
-def main():
-    pygame.init()
+        time.sleep(1)
+        while True:
+            data = stream.read(CHUNK, exception_on_overflow = False)
+            rms = audioop.rms(data, 2)
+            # Scale the rms value to be within 0-255
+            rmsS = rms * 0.014
+            if rmsS < colourMax:
+                yield rmsS
+    finally:
+        p.terminate()
+        stream.stop_stream()
+        stream.close()
+
+# ----------------------------------------------------------------------------
+def draw_func(pos_1_x,
+              pos_1_y,
+              pos_2_x,
+              pos_2_y,
+              pos_3_x,
+              pos_3_y,
+              colourRand,
+              rmsInt):
+    width = rmsInt
+    pyg.draw.polygon(screen,
+                        ((rmsInt+colourRand)*0.3, rmsInt, rmsInt),
+                        ((pos_1_x, pos_1_y),
+                         (pos_2_x, pos_2_y),
+                         (pos_3_x, pos_3_y)),
+                        width)
+
+    pyg.display.update()
+
+    if pos_3_x < LIMIT:
+        draw_func((pos_1_x-(pos_3_x/2), pos_1_y-(pos_3_y/2)),
+                  (pos_2_x-(pos_3_x/2), pos_2_y-(pos_3_y/2)),
+                  (pos_3_x-(pos_1_x/2), pos_3_y-(pos_1_y/2)),
+                  colourRand,
+                  rmsInt)
+        draw_func((pos_1_x+(pos_3_x/2), pos_1_y+(pos_3_y/2)),
+                  (pos_2_x-(pos_3_x/2), pos_2_y-(pos_3_y/2)),
+                  (pos_3_x-(pos_1_x/2), pos_3_y-(pos_1_y/2)),
+                  colourRand,
+                  rmsInt)
+        draw_func((pos_1_x+(pos_3_x/2), pos_1_y+(pos_3_y/2)),
+                  (pos_2_x+(pos_3_x/2), pos_2_y+(pos_3_y/2)),
+                  (pos_3_x-(pos_1_x/2), pos_3_y-(pos_1_y/2)),
+                  colourRand,
+                  rmsInt)
+
+# ----------------------------------------------------------------------------
+def webcam_func(cap):
+    # Capture frame by frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+    frame = np.rot90(frame)
+    webcam_frame = pyg.surfarray.make_surface(frame)
+    return(webcam_frame)
+# ----------------------------------------------------------------------------
+if __name__ == "__main__":
+    LIMIT = 200
+    gain_ = 1000
+
+    # init pyg, random and rms rmsFunction
+    pyg.init()
+    time.sleep(1)
     randomNumber = random.seed()
     rms = rmsFunction()
 
+    # get camera object
+    cap = cv2.VideoCapture(0)
+
     #create fullscreen display
-    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-    screenW = screen.get_width()
-    screenH = screen.get_height()
+    screen = pyg.display.set_mode((0,0), pyg.FULLSCREEN)
+    s_W = screen.get_width()
+    s_H = screen.get_height()
 
-    # Iterate the rms value and update the pyGame display
+    # Hide the mousey
+    pyg.mouse.set_visible(False)
+
+    # Iterate the rms value and update the pyg display
     for rmsValue in rms:
-        rmsInt = int(rmsValue)
-        sOneW = random.getrandbits(10)
-        sOneH = random.getrandbits(10)
-        sTwoW = random.getrandbits(10)
-        sTwoH = random.getrandbits(10)
-        sThreeW = random.getrandbits(10)
-        sThreeH = random.getrandbits(10)
-
-        posOneW = (sOneW + screenW) * 0.2
-        posOneH = (sOneH + screenH) * 0.2
-        posTwoW = (sTwoW + screenW) * 0.5
-        posTwoH = (sTwoH + screenH) * 0.5
-        posThreeW = (sThreeW + screenW) * 0.5
-        posThreeH = (sThreeH + screenH) * 0.5
-
         colourRand = random.getrandbits(8)
+        # webcam rmsFunction
+        webcam = webcam_func(cap)
+        screen.blit(webcam, (0,0))
 
         # Draw polygons to the screen
-        if rmsValue > 100:
-            pygame.draw.polygon(screen, 
-                               ((rmsInt+colourRand)*0.3, rmsInt, rmsInt), 
-                                 ((posOneW,posOneH), (posTwoW,posTwoH), (posThreeW,posThreeH)))
+        if rmsValue > 25:
+            rmsInt = int(rmsValue)
+            pos_1_x = random.getrandbits(10) - (s_W/2)
+            pos_1_y = random.getrandbits(8) + (s_H/2)
+            pos_2_x = random.getrandbits(8) + (s_W/2)
+            pos_2_y = random.getrandbits(10) - (s_H/2)
+            pos_3_x = random.getrandbits(10) + (s_W/2)
+            pos_3_y = random.getrandbits(10) + (s_H/2)
 
-        pygame.display.update()
+            screen.blit(webcam, (0,0))
+
+            draw_func(pos_1_x, pos_1_y,
+                      pos_2_x, pos_2_y,
+                      pos_3_x, pos_3_y,
+                      colourRand,
+                      rmsInt)
+            pyg.display.update()
+
+        pyg.display.update()
+        pyg.Surface.fill(screen, (0,0,0))
+
         time.sleep(0.01)
-        pygame.Surface.fill(screen, (0,0,0))
 
         # check for quit events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                p.terminate()
-                pygame.quit()
-                stream.stop_stream()
-                stream.close()
-            if event.type == pygame.KEYDOWN:
-                p.terminate()
-                pygame.quit()
-                stream.stop_stream()
-                stream.close()
-
-if __name__ == "__main__": main()
+        for event in pyg.event.get():
+            if event.type == pyg.QUIT:
+                pyg.quit()
+                cap.release()
+                cv2.destroyAllWindows()
+            if event.type == pyg.KEYDOWN:
+                pyg.quit()
+                cap.release()
+                cv2.destroyAllWindows()
+# ----------------------------------------------------------------------------
